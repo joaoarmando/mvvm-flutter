c3428247b36f301c50ea038a03f2f3dabb4258eb
Joao Armando <joaoaarmando@hotmail.com>
2021-04-18 17:24:59 -0300

    Show user details in the HomeView restoring the details from AppStore

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -8,8 +8,17 @@ class HomeView extends StatelessWidget {
     var store = Provider.of<AppStore>(context);
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: Text(store.name),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Image.network(store.picture),
+          SizedBox(height: 12),
+          Text(store.name),
+          SizedBox(height: 12),
+          Text(store.email),
+          SizedBox(height: 12),
+        ],
       ),
     );
   }
